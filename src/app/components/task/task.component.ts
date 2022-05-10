519540a2c84f3c82b280f5dbb251241de6a28ba7
lewyino <mateusz.lewandowski@gmail.com>
2022-05-10 13:08:42 +0200

    add components Task and TaskList

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {TaskModel} from "../../models/task-model";
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+export class TaskComponent implements OnInit {
+
+  @Input() task: TaskModel | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
